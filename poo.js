5ece6797ad9053b085030c9917cb7ccd62550b07
Levi3141592 <139075580+Levi3141592@users.noreply.github.com>
2023-08-31 11:24:01 -0300

    Update poo.js

--- poo.js
+++ poo.js
@@ -1,80 +1,61 @@
-// slides de promoção, abaixo do header
+class SlideManager {
+    constructor() {
+        this.position = 1;
+        this.allSlide = document.querySelector('#slide-both');
+        this.arrowL = document.querySelector('#arrow-left');
+        this.arrowR = document.querySelector('#arrow-right');
+        
+        this.arrowL.addEventListener('click', () => this.moveLeft());
+        this.arrowR.addEventListener('click', () => this.moveRight());
+    }
 
-var position_slide = 1; //posicao inicial = 1 
-         const all_slide = document.querySelector('#slide-both'); //variavel que mexera os slides 
-         const arrowL = document.querySelector('#arrow-left') 
-         const arrowR = document.querySelector('#arrow-right') 
-  
-              arrowL.addEventListener('click', ()=>{ 
-                 position_slide -= 1; 
-                 switch (position_slide){ 
-                     case 1:  
-                         arrowL.style.visibility = 'hidden'; 
-                         arrowR.style.visibility = 'visible' 
-                         all_slide.style.translate ='0'; 
-                     break; 
-                     case 2: 
-                         arrowL.style.visibility = 'visible'; 
-                         arrowR.style.visibility = 'visible' 
-                         all_slide.style.translate ='-100vw'; 
-                     break; 
-                     case 3:  
-                         arrowL.style.visibility = 'visible'; 
-                         arrowR.style.visibility = 'hidden'; 
-                         all_slide.style.translate ='-200vw'; 
-                     break; 
-                     default: 
-                         console.log('deu erro, a posicao do slide é diferente do esperado') 
-                         position_slide += 1; //cancelar a adição anterior 
-                     break; 
-                     } 
-                 }) 
-  
-                 
-  
-             arrowR.addEventListener('click', ()=>{ 
-                 position_slide += 1; 
-                 switch (position_slide){ 
-                     case 1:  
-                         arrowL.style.visibility = 'hidden'; 
-                         arrowR.style.visibility = 'visible' 
-                         all_slide.style.translate ='0'; 
-                     break; 
-                     case 2: 
-                         arrowL.style.visibility = 'visible' 
-                         arrowR.style.visibility = 'visible' 
-                         all_slide.style.translate ='-100vw'; 
-                     break; 
-                     case 3:  
-                         arrowL.style.visibility = 'visible' 
-                         arrowR.style.visibility = 'hidden' 
-                         all_slide.style.translate ='-200vw'; 
-                     break; 
-                     default: 
-                         console.log('deu erro, a posicao do slide é diferente do esperado') 
-                         position_slide -= 1; //cancelar a adição anterior 
-                     break; 
-                 } 
-             }) 
-  
-         const allsec = document.querySelector('#allsecs') //elemento que controla os slides2 
-         const arrows2 = document.querySelector('.arrows2') 
-         const arrowright2 = document.querySelector('#arrow-right2') 
-  
-         arrowright2.addEventListener('click',()=>{ 
-             allsec.style.transform = 'translateX(-100vw)'; // faz mexer 
-  
-             arrowright2.style.display = 'none'; //mudar visibilidade 
-             arrowleft2.style.display = 'block'; //mudar visibilidade 
-             arrows2.style.justifyContent = 'start';//muda a posição das setas 
-             console.log('right clicado'); 
-         }) 
-         const arrowleft2 = document.querySelector('#arrow-left2') 
-         arrowleft2.addEventListener('click',()=>{ 
-             allsec.style.transform = 'translateX(0vw)'; //faz mexer 
-             //mudar visibilidade 
-             arrowright2.style.display = 'block'; //mudar visibilidade 
-             arrowleft2.style.display = 'none'; //mudar visibilidade 
-             arrows2.style.justifyContent = 'end'; //muda a posição das setas 
-             console.log('left clicado'); 
-         })
+    moveLeft() {
+        if (this.position > 1) {
+            this.position--;
+            this.updateSlide();
+        }
+    }
+
+    moveRight() {
+        if (this.position < 3) {
+            this.position++;
+            this.updateSlide();
+        }
+    }
+
+    updateSlide() {
+        const translateValue = (this.position - 1) * -100;
+        this.allSlide.style.transform = `translateX(${translateValue}vw)`;
+        this.arrowL.style.visibility = this.position === 1 ? 'hidden' : 'visible';
+        this.arrowR.style.visibility = this.position === 3 ? 'hidden' : 'visible';
+    }
+}
+
+class SecondarySlideManager {
+    constructor() {
+        this.allSec = document.querySelector('#allsecs');
+        this.arrows2 = document.querySelector('.arrows2');
+        this.arrowRight2 = document.querySelector('#arrow-right2');
+        this.arrowLeft2 = document.querySelector('#arrow-left2');
+
+        this.arrowRight2.addEventListener('click', () => this.moveRight());
+        this.arrowLeft2.addEventListener('click', () => this.moveLeft());
+    }
+
+    moveLeft() {
+        this.allSec.style.transform = 'translateX(0vw)';
+        this.arrowRight2.style.display = 'block';
+        this.arrowLeft2.style.display = 'none';
+        this.arrows2.style.justifyContent = 'end';
+    }
+
+    moveRight() {
+        this.allSec.style.transform = 'translateX(-100vw)';
+        this.arrowRight2.style.display = 'none';
+        this.arrowLeft2.style.display = 'block';
+        this.arrows2.style.justifyContent = 'start';
+    }
+}
+
+const slideManager = new SlideManager();
+const secondarySlideManager = new SecondarySlideManager();
